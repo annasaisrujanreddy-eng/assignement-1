a244567ff657026e671161aa74b7a7d63efc56b5
annasaisrujanreddy <annasaisrujanreddy@gmail.com>
2025-09-15 21:33:19 +0530

    initial commit

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+from urllib.response import addbase
+
+a=input('first number: ')
+b=input('second number: ')
+a=float(a)
+b=float(b)
+print("addition: ",a+b)
+print('subtraction: ',a-b)
+print('multiplication: ',a*b)
+print("division: ",a/b)
+
+c=input('enter your first name: ')
+d=input('enter your second name: ')
+print('hello ',c+d,'welcome to python')
+git addbase
---
+++ sample program.py
@@ -0,0 +1,26 @@
+c=input("celcius: ")
+c=float(c)
+f=(c* 9/5) + 32
+print('farenheit: ',f)
+f=input('farenheit: ')
+f=float(f)
+c=(f-32) * 5/9
+print('celcius: ',c)
+kg=input("kilogram: ")
+kg=float(kg)
+lbs=(kg*2.2)
+print('pounds: ',lbs)
+lbs=input('pounds: ')
+lbs=float(lbs)
+kg=(lbs/2.2)
+print('kilogram: ',kg)
+cm=input('centimeter: ')
+cm=float(cm)
+inches=(cm/2.5)
+print('inches: ',inches)
+
+
+
+print(max(1,5,7,89,100))
+print(min(1,5,7,89,100))
+print(pow(2,2))
